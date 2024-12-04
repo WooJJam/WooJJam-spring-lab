855963411b4409bf849e96502a18e871f7a9001b
WooJJam <111514410+WooJJam@users.noreply.github.com>
2024-12-04 23:28:29 +0900

    feat: SchedulerConfigurer 으로 ThreadPool 설정

---
+++ schedule/spring-scheduler/src/main/java/com/woojjam/schedule/springscheduler/SchedulerConfigurer.java
@@ -0,0 +1,23 @@
+package com.woojjam.schedule.springscheduler;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfigurer implements SchedulingConfigurer {
+
+	@Override
+	public void configureTasks(final ScheduledTaskRegistrar taskRegistrar) {
+		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+
+		int threadCount = Runtime.getRuntime().availableProcessors();
+		threadPoolTaskScheduler.setPoolSize(threadCount);
+		threadPoolTaskScheduler.setThreadNamePrefix("my-scheduler-");
+		threadPoolTaskScheduler.initialize();
+		taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+	}
+}
