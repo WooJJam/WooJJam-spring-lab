c15643835010a7caba30a361c5186483d2628216
WooJJam <111514410+WooJJam@users.noreply.github.com>
2024-12-17 13:58:50 +0900

    chore: notification 모듈 분리

--- notification/src/main/java/co/kr/woojjam/NotificationApplication.java
+++ notification/src/main/java/co/kr/woojjam/NotificationApplication.java
@@ -1,4 +1,4 @@
-package co.kr.woojjam.notification;
+package co.kr.woojjam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
