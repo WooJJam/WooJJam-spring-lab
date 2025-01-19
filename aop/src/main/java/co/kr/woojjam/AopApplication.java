0c35fbf53430805d7884cc609a1fadec5ddcceb7
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-01-20 00:24:31 +0900

    add AOP module for logging

--- aop/src/main/java/co/kr/woojjam/AopApplication.java
+++ aop/src/main/java/co/kr/woojjam/AopApplication.java
@@ -1,13 +1,14 @@
-package com.woojjam.schedule.springscheduler;
+package co.kr.woojjam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class SpringSchedulerApplication {
+public class AopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSchedulerApplication.class, args);
+		SpringApplication.run(AopApplication.class, args);
 	}
 
 }
--- schedule/spring-scheduler/src/main/java/com/woojjam/schedule/springscheduler/domain/CustomMatchRepository.java
+++
@@ -1,8 +0,0 @@
-package com.woojjam.schedule.springscheduler.domain;
-
-import java.util.List;
-
-public interface CustomMatchRepository {
-
-	List<Match> findMatch();
-}
