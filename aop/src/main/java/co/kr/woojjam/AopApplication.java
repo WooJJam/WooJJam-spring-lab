fe6e826a8485a57fd52207fed64bb4a3a21f2dd8
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-01-20 14:49:08 +0900

    add JpaRepository for user entity

--- aop/src/main/java/co/kr/woojjam/AopApplication.java
+++ aop/src/main/java/co/kr/woojjam/AopApplication.java
@@ -1,8 +1,7 @@
-package co.kr.woojjam.aop;
+package co.kr.woojjam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class AopApplication {
