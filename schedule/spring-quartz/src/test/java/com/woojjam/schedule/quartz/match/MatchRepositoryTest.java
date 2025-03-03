84cdb4bb3fefac2154d2c8b2fe34acebfa907f86
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-03-04 00:28:40 +0900

    chore: 통합 테스트 삭제

--- schedule/spring-quartz/src/test/java/com/woojjam/schedule/quartz/match/MatchRepositoryTest.java
+++ schedule/spring-quartz/src/test/java/com/woojjam/schedule/quartz/match/MatchRepositoryTest.java
@@ -1,46 +1,46 @@
-package com.woojjam.schedule.quartz.match;
-
-
-import static org.assertj.core.api.Assertions.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.woojjam.schedule.quartz.domain.Match;
-import com.woojjam.schedule.quartz.domain.MatchRepository;
-
-@SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class MatchRepositoryTest {
-
-	@Autowired
-	private MatchRepository matchRepository;
-
-
-	@BeforeEach
-	public void setUp() throws Exception {
-		LocalDateTime startAt = LocalDateTime.now();
-		LocalDateTime endAt = startAt.plusHours(2);
-		Match match = Match.builder()
-			.total(10)
-			.remain(0)
-			.startAt(startAt.plusMinutes(30))
-			.endAt(endAt.plusMinutes(30))
-			.build();
-		matchRepository.save(match);
-
-		Match match2 = Match.builder()
-			.total(10)
-			.remain(2)
-			.startAt(startAt)
-			.endAt(endAt)
-			.build();
-		matchRepository.save(match2);
-    }
-}
+// package com.woojjam.schedule.quartz.match;
+//
+//
+// import static org.assertj.core.api.Assertions.*;
+//
+// import java.time.LocalDateTime;
+// import java.util.List;
+//
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// import com.woojjam.schedule.quartz.domain.Match;
+// import com.woojjam.schedule.quartz.domain.MatchRepository;
+//
+// @SpringBootTest
+// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+// class MatchRepositoryTest {
+//
+// 	@Autowired
+// 	private MatchRepository matchRepository;
+//
+//
+// 	@BeforeEach
+// 	public void setUp() throws Exception {
+// 		LocalDateTime startAt = LocalDateTime.now();
+// 		LocalDateTime endAt = startAt.plusHours(2);
+// 		Match match = Match.builder()
+// 			.total(10)
+// 			.remain(0)
+// 			.startAt(startAt.plusMinutes(30))
+// 			.endAt(endAt.plusMinutes(30))
+// 			.build();
+// 		matchRepository.save(match);
+//
+// 		Match match2 = Match.builder()
+// 			.total(10)
+// 			.remain(2)
+// 			.startAt(startAt)
+// 			.endAt(endAt)
+// 			.build();
+// 		matchRepository.save(match2);
+//     }
+// }
