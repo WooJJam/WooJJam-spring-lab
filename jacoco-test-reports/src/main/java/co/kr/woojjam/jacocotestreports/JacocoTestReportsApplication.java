6f04ac9676c550ce22d4f07097a12793f93abfee
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-03-04 12:49:01 +0900

    chore: jacoco-reports 모듈 생

---
+++ jacoco-test-reports/src/main/java/co/kr/woojjam/jacocotestreports/JacocoTestReportsApplication.java
@@ -0,0 +1,13 @@
+package co.kr.woojjam.jacocotestreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacocoTestReportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacocoTestReportsApplication.class, args);
+	}
+
+}
