8af0f7504669e5f965c52336b7f36e2a5fdeb636
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-03-09 18:55:35 +0900

    test: 낙관적락 테스트 작

--- concurrency/src/test/java/co/kr/woojjam/concurrency/coupon/TestCouponLockTest.java
+++ concurrency/src/test/java/co/kr/woojjam/concurrency/coupon/TestCouponLockTest.java
@@ -1,17 +1,27 @@
 package co.kr.woojjam.concurrency.coupon;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.util.StopWatch;
 
 import co.kr.woojjam.concurrency.config.TestDataBaseConfig;
+import jakarta.persistence.OptimisticLockException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootTest(properties = {"spring.jpa.hibernate.ddl-auto=create"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-// @ContextConfiguration(classes = TestJpaConfig.class)
 public class TestCouponLockTest extends TestDataBaseConfig {
 
 	@Autowired
@@ -26,17 +36,50 @@ public class TestCouponLockTest extends TestDataBaseConfig {
 		testCoupon = TestCoupon.builder()
 			.code("A")
 			.discountAmount(1000)
-			.stock(100)
+			.stock(2)
 			.build();
 
 		testCouponRepository.save(testCoupon);
 	}
 
 	@Test
-	public void 쿠폰_차감_테스트() throws Exception{
-		testCouponService.useCoupon(1L);
+	public void 낙관적락_유리한_케이스_테스트() throws InterruptedException {
+		int users = 5;
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
+		CountDownLatch countDownLatch = new CountDownLatch(5);
+		AtomicInteger success = new AtomicInteger(0);
+		AtomicInteger failed = new AtomicInteger(0);
+
+		long startTime = System.currentTimeMillis();
+
+		for (int i = 0; i < users; i++) {
+			executorService.submit(() -> {
+				while(true) {
+					try {
+						testCouponService.useCoupon(1L);
+						success.incrementAndGet();
+						break;
+					} catch (ObjectOptimisticLockingFailureException e) {
+						log.warn("동시성 충돌 발생");
+					} catch (IllegalStateException e) {
+						log.info("{}",e.getMessage());
+						failed.incrementAndGet();
+						break;
+					}
+				}
+				countDownLatch.countDown();
+			});
+		}
+
+		countDownLatch.await(); // 모든 스레드가 끝날 때까지 대기
+		long endTime = System.currentTimeMillis();
+
+		System.out.println("낙관적 락 테스트 실행 시간: " + (endTime - startTime) + "ms");
 
 		TestCoupon coupon = testCouponService.read(1L);
+		assertThat(coupon.getStock()).isEqualTo(0);
+		assertThat(success.get()).isEqualTo(2);
+		assertThat(failed.get()).isEqualTo(3);
 	}
 
 }
--- concurrency/src/test/java/co/kr/woojjam/concurrency/coupon/TestCouponService.java
+++ concurrency/src/test/java/co/kr/woojjam/concurrency/coupon/TestCouponService.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TestCouponService {
@@ -16,6 +18,7 @@ public class TestCouponService {
 		TestCoupon coupon = testCouponRepository.findById(couponId)
 			.orElseThrow(() -> new IllegalArgumentException("쿠폰이 존재하지 않습니다."));
 		coupon.useCoupon();
+		log.info("coupon = {}", coupon);
 	}
 
 	public TestCoupon read(Long id) {
