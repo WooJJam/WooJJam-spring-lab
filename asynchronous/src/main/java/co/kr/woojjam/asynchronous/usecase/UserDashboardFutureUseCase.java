62b93938c6a31fb5b2e87c12f36c72bfa75eb2ba
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-07-01 15:17:58 +0900

    feat: Future로 대시보드 API 생성

---
+++ asynchronous/src/main/java/co/kr/woojjam/asynchronous/usecase/UserDashboardFutureUseCase.java
@@ -0,0 +1,36 @@
+package co.kr.woojjam.asynchronous.usecase;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.springframework.stereotype.Component;
+
+import co.kr.woojjam.asynchronous.service.UserDashboardFutureService;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class UserDashboardFutureUseCase {
+
+	private final UserDashboardFutureService userDashboardFutureService;
+
+	public void readDashboard() throws ExecutionException, InterruptedException {
+		long startTime = System.currentTimeMillis();
+		Future<String> userFuture = userDashboardFutureService.readUser();
+		Future<String> orderFuture = userDashboardFutureService.readOrders();
+
+		String user = userFuture.get();
+		String orders = orderFuture.get();
+
+		long endTime = System.currentTimeMillis();
+
+		combine(user, orders, endTime - startTime);
+	}
+
+	private void combine(final String user, final String orders, long currentTime) {
+		System.out.println("\n📋 사용자 대시보드 정보:");
+		System.out.println("User: " + user);
+		System.out.println("Recent Orders:\n" + orders);
+		System.out.println("\n총 소요 시간: " + currentTime + "ms");
+	}
+}
