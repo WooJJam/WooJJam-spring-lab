67c631f26b3d22a8fa9c1fab910c9d30381658ff
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-07-11 11:01:58 +0900

    test: thenCombine 테스트 코드 작성

--- asynchronous/src/test/java/co/kr/woojjam/asynchronous/CompletableFutureOrderCombineTest.java
+++ asynchronous/src/test/java/co/kr/woojjam/asynchronous/CompletableFutureOrderCombineTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 public class CompletableFutureOrderCombineTest {
 
 	private static final String orderNo = "1234567";
+	private static final String optionNo = "2222";
 	private Map<String, String> items = new HashMap<>();
 
 	@BeforeEach
@@ -29,6 +30,19 @@ public class CompletableFutureOrderCombineTest {
 			item -> CompletableFuture.supplyAsync(() -> getPaymentInfo(item)));
 
 		assertThat("주문 아이템: iPhone 16/ 결제 정보: 신용카드 1,200,000원").isEqualTo(result.get());
+	}
+
+	@Test
+	@DisplayName("thenCombine 으로 작업 결합하기")
+	void thenCombineTest() throws ExecutionException, InterruptedException {
+		CompletableFuture<String> orderFuture = CompletableFuture.supplyAsync(() -> orderItem(orderNo));
+		CompletableFuture<String> optionFuture = CompletableFuture.supplyAsync(this::getProductInfo);
+
+		CompletableFuture<String> result = orderFuture.thenCombine(optionFuture,
+			this::combineOrderAndProductInfo);
+
+
+		assertThat("주문 아이템: iPhone 16/ [옵션] AppleCare+").isEqualTo(result.get());
 	}
 
 	private String orderItem(final String orderNo) {
@@ -49,6 +63,14 @@ public class CompletableFutureOrderCombineTest {
 			System.out.println(e.getCause().getMessage());
 		}
 		return orderItem + "/ 결제 정보: 신용카드 1,200,000원";
+	}
+
+	private String getProductInfo() {
+		return "AppleCare+";
+	}
+
+	private String combineOrderAndProductInfo(String orderNo, String product) {
+		return orderNo + "/ [옵션] " + product;
 	}
 
 }
