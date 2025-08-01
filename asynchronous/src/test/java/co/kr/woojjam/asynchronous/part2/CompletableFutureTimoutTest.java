c41c7eadff7a20fe00edc0f8c48a0795a0ef0dc6
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-08-01 14:22:26 +0900

    test: CompletableFuture orTimeout 메소드로 타임아웃 테스트

--- asynchronous/src/test/java/co/kr/woojjam/asynchronous/part2/CompletableFutureTimoutTest.java
+++ asynchronous/src/test/java/co/kr/woojjam/asynchronous/part2/CompletableFutureTimoutTest.java
@@ -1,5 +1,7 @@
 package co.kr.woojjam.asynchronous.part2;
 
+import static org.assertj.core.api.Assertions.*;
+
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -29,5 +31,23 @@ public class CompletableFutureTimoutTest {
 		} catch (TimeoutException e) {
 			System.out.println("타임아웃 발생!");
 		}
+	}
+
+	@Test
+	@DisplayName("orTimeout()으로 타임아웃 설정하기")
+	void orTimeout_타임아웃_설정() throws ExecutionException, InterruptedException {
+
+		CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
+				try {
+					Thread.sleep(3000);
+					return "응답 완료";
+				} catch (InterruptedException e) {
+					throw new RuntimeException(e);
+				}
+			}).orTimeout(1, TimeUnit.SECONDS)
+			.exceptionally(ex -> "타임아웃 기본값");
+
+		assertThat("타임아웃 기본값").isEqualTo(future.get()); // → "타임아웃 기본값"
+
 	}
 }
