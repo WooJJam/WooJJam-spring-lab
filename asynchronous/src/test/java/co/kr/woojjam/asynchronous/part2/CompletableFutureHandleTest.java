6790290ff495aa8491d52145b97af1c24991f61d
WooJJam <111514410+WooJJam@users.noreply.github.com>
2025-08-01 13:08:05 +0900

    test: handle 메소드 예외 처리 테스트 코드 작성

--- asynchronous/src/test/java/co/kr/woojjam/asynchronous/part2/CompletableFutureHandleTest.java
+++ asynchronous/src/test/java/co/kr/woojjam/asynchronous/part2/CompletableFutureHandleTest.java
@@ -10,14 +10,41 @@ import org.junit.jupiter.api.Test;
 public class CompletableFutureHandleTest {
 
 	@Test
-	@DisplayName("handle로 예외 처리 및 결과 조작하기")
-	void 주문_정보_조회시_예외_처리() throws ExecutionException, InterruptedException {
+	@DisplayName("handle 정상 처리")
+	void handle로_정상_처리() throws ExecutionException, InterruptedException {
+
+		String code = "1234";
 
 		CompletableFuture.supplyAsync(() -> {
-			if (new Random().nextBoolean()) {
+			if (code == null) {
 				throw new IllegalArgumentException("랜덤 예외 발생");
 			}
-			return "정상 결과";
+
+			return "정상 처리";
+		}).handle((result, ex) -> {
+			if (ex != null) {
+				System.out.println("예외 처리: " + ex.getMessage());
+				return "예외 발생 시 기본값 반환";
+			}
+
+			return result;
+		}).thenAccept((result -> {
+			System.out.println("최종 결과: " + result);
+		}));
+	}
+
+	@Test
+	@DisplayName("handle 예외 처리")
+	void handle로_예외_처리() throws ExecutionException, InterruptedException {
+
+		String code = "1234";
+
+		CompletableFuture.supplyAsync(() -> {
+			if (code != null) {
+				throw new IllegalArgumentException("예외 발생");
+			}
+
+			return "정상 처리";
 		}).handle((result, ex) -> {
 			if (ex != null) {
 				System.out.println("예외 처리: " + ex.getMessage());
